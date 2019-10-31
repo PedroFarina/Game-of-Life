605330db2504c6237f10c29775a4f363fe52322f
Pedro Farina <pedrogfarina@gmail.com>
2019-10-31 15:15:38 -0300

    Setting up scene

---
+++ Game of Life/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  Game of Life
+//
+//  Created by Pedro Giuliano Farina on 31/10/19.
+//  Copyright © 2019 Pedro Giuliano Farina. All rights reserved.
+//
+
+import SceneKit
+
+public class GameScene: SCNScene {
+    
+}
